ea526e6ad92064ed7ea5a00303fd7647bdbf11b8
PaolaBramlett <paolabramlett@gmail.com>
2023-04-21 14:54:22 -0600

    p. navbar tecnicamente lista

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,86 +1,148 @@
-import { useState } from "react";
-import Link from "next/link";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faUser, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useState } from 'react';
+import Link from 'next/link';
+import { FaUser } from 'react-icons/fa';
 
-const Navbar = ({ isLoggedIn }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+    setShowMenu(!showMenu);
   };
 
-  const closeMenu = () => {
-    setIsMenuOpen(false);
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    // aquí va la lógica del logout... pendiente
   };
 
   return (
     <nav className="navbar">
-      <div className="navbar__logo">
+      <div className="navbar-container">
         <Link legacyBehavior href="/">
-          <a>
-            <span>Nomadapp</span>
-          </a>
-        </Link >
-      </div>
+          <a className="navbar-logo">NomadApp</a>
+        </Link>
 
-      <div className="navbar__menu" onClick={toggleMenu}>
-        {isMenuOpen ? (
-          <FontAwesomeIcon icon={faTimes} />
-        ) : (
-          <FontAwesomeIcon icon={faBars} />
-        )}
-      </div>
+        <div className="navbar-links">
+          <Link legacyBehavior href="/about">
+            <a>About</a>
+          </Link>
 
-      <div className={`navbar__Link legacyBehaviors ${isMenuOpen ? "active" : ""}`}>
-        <Link legacyBehavior href="/about">
-          <a onClick={closeMenu}>
-            <span>About</span>
-          </a>
-        </Link >
-        <Link legacyBehavior href="/login">
-          <a onClick={closeMenu}>
-            <span>Login</span>
-          </a>
-        </Link >
+          {!isLoggedIn && (
+            <Link legacyBehavior href="/login">
+              <a>Login</a>
+            </Link>
+          )}
 
-        {isLoggedIn && (
-          <>
-            <Link legacyBehavior href="/dashboard">
-              <a onClick={closeMenu}>
-                <span>Dashboard</span>
-              </a>
-            </Link >
-            <div className="navbar__user">
-              <FontAwesomeIcon icon={faUser} />
-              <div className="navbar__dropdown">
-                <Link legacyBehavior href="/account-settings">
-                  <a onClick={closeMenu}>
-                    <span>Account Settings</span>
-                  </a>
-                </Link >
-                <Link legacyBehavior href="/favorites">
-                  <a onClick={closeMenu}>
-                    <span>Favorites</span>
-                  </a>
-                </Link >
-                <Link legacyBehavior href="/likes">
-                  <a onClick={closeMenu}>
-                    <span>Likes</span>
-                  </a>
-                </Link >
-                <Link legacyBehavior href="/logout">
-                  <a onClick={closeMenu}>
-                    <span>Log Out</span>
-                  </a>
-                </Link>
+          {isLoggedIn && (
+            <>
+              <Link legacyBehavior href="/dashboard">
+                <a>Dashboard</a>
+              </Link>
+
+              <div className="navbar-dropdown">
+                <div className="navbar-dropdown-toggle" onClick={toggleMenu}>
+                  <FaUser />
+                </div>
+
+                {showMenu && (
+                  <div className="navbar-dropdown-menu">
+                    <Link legacyBehavior href="/account-settings">
+                      <a>Account Settings</a>
+                    </Link>
+                    <Link legacyBehavior href="/favorites">
+                      <a>Favorites</a>
+                    </Link>
+                    <Link legacyBehavior href="/likes">
+                      <a>Likes</a>
+                    </Link>
+                    <button onClick={handleLogout}>Log out</button>
+                  </div>
+                )}
               </div>
-            </div>
-          </>
-        )}
+            </>
+          )}
+        </div>
       </div>
+
+      <style jsx>{`
+        .navbar {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          background-color: #FFFFFF;
+          padding: 1rem;
+          position: fixed;
+          top: 0;
+          left: 0;
+          right: 0;
+        }
+
+        .navbar-container {
+          display: flex;
+          align-items: center;
+        }
+
+        .navbar-logo {
+          font-weight: bold;
+          font-size: 1.5rem;
+          color: #000000;
+          margin-right: 2rem;
+        }
+
+        .navbar-links a {
+          color: #000000;
+          margin-right: 1.5rem;
+          transition: color 0.2s ease-in-out;
+        }
+
+        .navbar-links a:hover {
+          color: #FF0000;
+        }
+
+        .navbar-links {
+          display: flex;
+          align-items: center;
+        }
+
+        .navbar-dropdown {
+          position: relative;
+        }
+
+        .navbar-dropdown-toggle {
+          display: flex;
+          align-items: center;
+          margin-left: 1.5rem;
+          cursor: pointer;
+        }
+
+        .navbar-dropdown-toggle:hover {
+          color: #FF0000;
+        }
+
+        .navbar-dropdown-menu {
+          position: absolute;
+          top: 100%;
+          right: 0;
+          background-color: #FFFFFF;
+          border: 1px solid #000000;
+          padding: 1rem;
+          z-index: 1;
+        }
+
+        .navbar-dropdown-menu a {
+          color: #000000;
+          display: block;
+          margin-bottom: 0.5rem;
+          transition: color 0.2s ease-in-out;
+        }
+
+        .navbar-dropdown-menu a:hover {
+          color: #FF0000;
+        }
+      `}</style>
     </nav>
   );
+
 };
 
 export default Navbar;
